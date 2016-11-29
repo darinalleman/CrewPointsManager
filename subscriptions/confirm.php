606e92ccb7fa20730482dc99d61b34d28bce56e7
Darin Alleman <da8289@cs.ship.edu>
2016-11-29 10:45:57 -0500

    finalized subscribtions, confirm and unsub

--- subscriptions/confirm.php
+++ subscriptions/confirm.php
@@ -9,14 +9,17 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/core-min.js"></script>
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/cipher-core.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
 </head>
 <body>
   <nav class="orange" role="navigation">
     <div  style="width:100%" class="nav-wrapper container"><a id="logo-container" 
          href="../" class="brand-logo"><img src=../Ship_logo.png>
       <ul class="right hide-on-med-and-down">
-        <li><a onclick="location.href='../teams/home.php'" >Teams</a></li>
-        <li><a onclick="location.href='../events/home.php'" >Events</a></li>
+        <li><a onclick="location.href='../teams/home.html'" >Teams</a></li>
+        <li><a onclick="location.href='../events/home.html'" >Events</a></li>
         <li><a onclick="location.href='home.php'">Subscriptions</a></li>
         <li><a href="../users/home.html"><i class="material-icons left">account_circle</i>Profile</a></li>
       </ul>
@@ -36,43 +39,46 @@
         <br>
       <h4 class="header center blue-text">Subscriptions</h1>
       <div class="row center">
-        <h5 class="header col s12 light">Subscribe to our weekly email updates to stay in the loop about all crew events!</h5>
+      <?php
+$id = $_GET['email'];
+
+if($id) {
+    $decrypted = openssl_decrypt($id, "aes-256-ctr", "7d49782f2d2e465a");
+    if (file_exists("../db_info/db_login_info.txt")) 
+    {
+        //open my secret file that has the username on the first line
+        $fh = fopen("../db_info/db_login_info.txt","r");
+        $user_name = fgets($fh);
+        //and the password on the second line
+        $pwd = fgets($fh); 
+        fclose($fh);
+        //clean up the input 
+        $user_name = trim(preg_replace('/\R+/', ' ',$user_name));
+        $pwd = trim(preg_replace('/\R+/', ' ',$pwd));
+        $db_host = 'webprog.cs.ship.edu';
+        $db = 'webprog29';
+        //connect to the database
+        $conn = new mysqli($db_host,$user_name,$pwd,$db);
+        if ($conn->connect_error) die($conn->connect_error);
+        else 
+        {
+            mysqli_query($conn,"INSERT INTO SUBSCRIPTIONS values ('".$decrypted."');");
+            echo "<h5 class=\"header col s12 light\">Thank you ".$decrypted." for subscribing to the Crew email updates.</h5>";
+            mysqli_commit($conn);
+        }
+    }
+}
+else {
+    echo '<p>No ID parameter.</p>';
+}
+
+?>
       </div>
       <div class="row center">
         <div style="width:50%; margin:auto;">
-            <?php
-            //if "email" variable is filled out, send email
-            if (isset($_REQUEST['email']))  {
-              $email = $_REQUEST['email'];
-              $crypt = openssl_encrypt($email, "aes-256-ctr", "7d49782f2d2e465a");
-              $admin_email = "crews-no-reply@cs.ship.edu";
-              $subject = "Confirm your Ship CS Department Crews subscription";
+            <script>
 
-              $body = "
-              Confirm your subscription: 
-              
-              webprog.cs.ship.edu/webprog29/subscriptions/confirm.php?email=".$crypt."
-              
-              If you did not request this email, you can safely ignore it.
-              
-
-              Unsubscribe: webprog.cs.ship.edu/webprog29/subscriptions/unsub.php?email=".$crypt."
-              ";
-            
-              //send email
-              mail($email, "$subject", $body, "From:" . $admin_email);
-            
-              //Email response
-              echo "<p>Check your email for a confirmation link.</p><br><br><br><br>";
-            }
-            else{
-              echo "
-            <form method=\"post\">
-                <input name=\"email\" placeholder=\"Your email address\" type=\"email\" />
-                <input class=\"btn-large waves-effect waves-light light-blue\" type=\"submit\" value=\"Subscribe\" />
-            </form>";
-            }
-          ?>
+            </script>
         </div>
           
       </div>
@@ -80,7 +86,7 @@
 
     </div>
   </div>
-
+  
   <footer class="page-footer grey darken-3">
     <div class="container">
       <div class="row">
@@ -99,6 +105,7 @@
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
   <script src="../js/materialize.js"></script>
 
   </body>
